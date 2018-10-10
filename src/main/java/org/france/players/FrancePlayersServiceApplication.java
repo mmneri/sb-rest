37ad432251a77355c114a1d27b57cd45499cd601
Marouen MNERI <mmneri@santeclair.fr>
2018-10-10 15:39:08 +0200

    update

--- src/main/java/org/france/players/FrancePlayersServiceApplication.java
+++ src/main/java/org/france/players/FrancePlayersServiceApplication.java
@@ -3,20 +3,20 @@ package org.france.players;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class FrancePlayersServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrancePlayersServiceApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(FrancePlayersServiceApplication.class, args);
+    }
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(FrancePlayersServiceApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder builder) {
+        return builder.sources(FrancePlayersServiceApplication.class);
+    }
 
 }
--- src/main/java/org/france/players/controller/PlayerController.java
+++ src/main/java/org/france/players/controller/PlayerController.java
@@ -10,21 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlayerController {
 
-	private ArrayList<Player> players;
+    private final ArrayList<Player> players;
 
-	public PlayerController() {
-		this.players = new ArrayList<Player>();
-		this.players.add(new Player(1, 10, "Kylian Mbappé", 19, "PSG", "Attaquant",
-				"https://www.fupa.net/fupa/images/photo/big/kylian-mbappe-878771_2.jpg"));
-	}
+    public PlayerController() {
+        this.players = new ArrayList<Player>();
+        this.players.add(new Player(1, 10, "Kylian Mbappé", 19, "PSG", "Attaquant",
+                        "https://www.fupa.net/fupa/images/photo/big/kylian-mbappe-878771_2.jpg"));
+        this.players.add(new Player(2, 6, "Paul Pgba", 25, "Manchester united", "Milieu terrain",
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Paul_Pogba_in_2018.jpg/275px-Paul_Pogba_in_2018.jpg"));
+    }
 
-	@GetMapping("/players")
-	public ArrayList<Player> getPlayers() {
-		return this.players;
-	}
+    @GetMapping("/players")
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
 
-	@GetMapping("/player/{id}")
-	public Player getPlayer(@PathVariable int id) {
-		return this.players.get(id);
-	}
+    @GetMapping("/player/{id}")
+    public Player getPlayer(@PathVariable final int id) {
+        return this.players.get(id);
+    }
 }
